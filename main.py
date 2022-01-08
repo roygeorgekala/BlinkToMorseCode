7a17fe5e8dee0a25c8d5295d523c1369b2492b17
RoyGeorge29 <roygeorgekala@gmail.com>
2022-01-08 16:44:04 +0530

    Short and Long blink detection

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import cv2
 
 # initializing modifiable values
-CAMERA_INPUT = 0
-BLINK_THRESHOLD = 6
+CAMERA_INPUT = 0  # Select which camera to use, 0 usually works for inbuilt webcams
+
+# Number of frames at 30fps where closed eyes indicate short blink aka a dot
+SHORT_BLINK_THRESHOLD = 3
+# Number of frames at 30fps where closed eyes indicate long blink aka a dash
+LONG_BLINK_THRESHOLD = 12
+
 FACE_HAAR_CASCADE = "haarcascade_frontalface_alt2.xml"
 EYE_HAAR_CASCADE = "haarcascade_eye_tree_eyeglasses.xml"
 
 
-# Initializing working variables
+# Initializing working variables (Non Modifiable)
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + FACE_HAAR_CASCADE)
 eye_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + EYE_HAAR_CASCADE)
 frame_count = 0
-blink_count = 0
-last_blink_status = True
+long_blink_count = 0
+short_blink_count = 0
+short_blink_status = True
+long_blink_status = True
+
 
 # Initalizing the video camera output
 capture = cv2.VideoCapture(CAMERA_INPUT)
@@ -50,11 +58,17 @@ while True:
             frame_count += 1
         else:
             frame_count = 0
-            last_blink_status = True
+            short_blink_status = True
+            long_blink_status = True
 
-        if last_blink_status and frame_count > BLINK_THRESHOLD:
-            last_blink_status = False
-            blink_count += 1
+        if frame_count > SHORT_BLINK_THRESHOLD and short_blink_status:
+            short_blink_count += 1
+            short_blink_status = False
+
+        elif long_blink_status and frame_count > LONG_BLINK_THRESHOLD:
+            long_blink_status = False
+            short_blink_count -= 1
+            long_blink_count += 1
             frame_count = 0
 
     # Rectangle for showing outputs
@@ -66,9 +80,10 @@ while True:
     content = 'FPS:' + str(capture.get(cv2.CAP_PROP_FPS))
     content += " || Eye Closed Time: " + \
         str("%.3f" % (frame_count * (1.0/30))) + " ms"
-    content += " || Number of Blinks: " + str(blink_count)
+    content += " || Long Blinks: " + str(long_blink_count)
+    content += " || Short Blinks: " + str(short_blink_count)
 
-    cv2.putText(frame, content, (50, height-20), font, 0.5,
+    cv2.putText(frame, content, (50, height-20), font, 0.4,
                 (0, 0, 0), 1, lineType=cv2.LINE_AA)
 
     # Frame resizing to make the output look bigger
