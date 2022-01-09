c65d232b16702ea1d8f4dc6c395794a9f82bd8c6
RoyGeorge29 <roygeorgekala@gmail.com>
2022-01-09 09:16:52 +0530

    Typing and spaces added

--- main.py
+++ main.py
@@ -16,98 +16,100 @@ FACE_HAAR_CASCADE = "haarcascade_frontalface_alt2.xml"
 EYE_HAAR_CASCADE = "haarcascade_eye_tree_eyeglasses.xml"
 
 
-# Initializing working variables (Non Modifiable)
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + FACE_HAAR_CASCADE)
-eye_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + EYE_HAAR_CASCADE)
-frame_count = 0
-open_eye_count = 0
-short_blink_status = True
-long_blink_status = True
-open_status = True
-content = ""
-read = ""
-# Initalizing the video camera output
-capture = cv2.VideoCapture(CAMERA_INPUT)
+def main():
 
-while True:
-    ret, frame = capture.read()
+    # Initializing working variables (Non Modifiable)
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + FACE_HAAR_CASCADE)
+    eye_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + EYE_HAAR_CASCADE)
+    frame_count = 0
+    open_eye_count = 0
+    short_blink_status = True
+    long_blink_status = True
+    open_status = True
+    content = ""
+    read = ""
+    # Initalizing the video camera output
+    capture = cv2.VideoCapture(CAMERA_INPUT)
 
-    # Flipping image to ensure mirroring
-    frame = cv2.flip(frame, 1)
+    while True:
+        ret, frame = capture.read()
 
-    # Height and width of video feed
-    width = int(capture.get(3))
-    height = int(capture.get(4))
+        # Flipping image to ensure mirroring
+        frame = cv2.flip(frame, 1)
 
-    # Grayscale image for facial  detection
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Height and width of video feed
+        width = int(capture.get(3))
+        height = int(capture.get(4))
 
-    faces = face_cascade.detectMultiScale(gray_frame, 1.2, 3)
+        # Grayscale image for facial  detection
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # TODO: Change from for loops to just singular, skips for if more than one face detected
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray_frame[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.2, 3)
+        faces = face_cascade.detectMultiScale(gray_frame, 1.2, 3)
 
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        # TODO: Change from for loops to just singular, skips for if more than one face detected
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            roi_gray = gray_frame[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray, 1.2, 3)
 
-    # Blink Detection algorithm
-    if len(faces) > 0:
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey),
+                              (ex+ew, ey+eh), (0, 255, 0), 2)
 
-        if len(eyes) == 0:
-            # Increment number of frames where eyes have been closed
-            frame_count += 1
-            open_eye_count = 0
-            open_status = True
-        else:
-            # Reset number of frames and blink status' when eyes are opened
-            frame_count = 0
-            short_blink_status = True
-            long_blink_status = True
-            open_eye_count += 1
+        # Blink Detection algorithm
+        if len(faces) > 0:
 
-            if open_eye_count > IN_BETWEEN_THRESHOLD and open_status:
+            if len(eyes) == 0:
+                # Increment number of frames where eyes have been closed
+                frame_count += 1
+                open_eye_count = 0
+                open_status = True
+            else:
+                # Reset number of frames and blink status' when eyes are opened
+                frame_count = 0
+                short_blink_status = True
+                long_blink_status = True
+                open_eye_count += 1
 
-                content += interpreter.interpret(read.strip())
-                read = ""
-                open_status = False
+                if open_eye_count > IN_BETWEEN_THRESHOLD and open_status:
 
-        if short_blink_status and frame_count > SHORT_BLINK_THRESHOLD:
-            read += ". "
-            short_blink_status = False
+                    content += interpreter.interpret(read.strip())
+                    read = ""
+                    open_status = False
 
-        elif long_blink_status and frame_count > LONG_BLINK_THRESHOLD:
-            long_blink_status = False
-            read = read[:-2]+"_ "
-            frame_count = 0
+            if short_blink_status and frame_count > SHORT_BLINK_THRESHOLD:
+                read += ". "
+                short_blink_status = False
 
-    # Rectangle for showing outputs
-    cv2.rectangle(frame, (10, height - 60),
-                  (width-10, height-10), (255, 0, 0), -1)
-    font = cv2.FONT_HERSHEY_SIMPLEX
+            elif long_blink_status and frame_count > LONG_BLINK_THRESHOLD:
+                long_blink_status = False
+                read = read[:-2]+"_ "
+                frame_count = 0
 
-    """# Displayed information on the screen
-        content = 'FPS:' + str(capture.get(cv2.CAP_PROP_FPS))
-        content += " || Eye Closed Time: " + str("%.3f" % (frame_count * (1.0/30))) + " ms"
-        content += " || Long Blinks: " + str(long_blink_count)
-        content += " || Short Blinks: " + str(short_blink_count)
-        content += " || Open eye frames: " + str(open_eye_count)
-    """
-    cv2.putText(frame, content+read, (50, height-20), font, 0.5,
-                (0, 0, 0), 1, lineType=cv2.LINE_AA)
+        # Rectangle for showing outputs
+        cv2.rectangle(frame, (10, height - 60),
+                      (width-10, height-10), (255, 0, 0), -1)
+        font = cv2.FONT_HERSHEY_SIMPLEX
 
-    # Frame resizing to make the output look bigger
-    frame = cv2.resize(frame, (0, 0), fx=1.5, fy=1.5)
-    cv2.imshow("Camera output", frame)
+        cv2.putText(frame, content+read, (50, height-20), font, 0.5,
+                    (0, 0, 0), 1, lineType=cv2.LINE_AA)
 
-    # Wait until Q is pressed to exit the application
-    if cv2.waitKey(1) == ord('q'):
-        break
+        # Frame resizing to make the output look bigger
+        frame = cv2.resize(frame, (0, 0), fx=1.5, fy=1.5)
+        cv2.imshow("Camera output", frame)
 
-capture.release()
-cv2.destroyAllWindows()
+        # Wait until Q is pressed to exit the application
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
+else:
+    print('Please do not import the script. ')
